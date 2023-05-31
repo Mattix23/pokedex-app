e2ab322dc82bc98642a18c98bd50db24a1ca8fb9
Mario Torres Jr <torres.mario590@gmail.com>
2023-05-31 12:56:30 -0500

    add user authentication, login and logout pages, update requirements

--- pokedex/views.py
+++ pokedex/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import redirect
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView
+from django.views.generic.edit import CreateView
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.forms import UserCreationForm
 
 from datetime import datetime
 
@@ -13,3 +16,19 @@ class HomeView(TemplateView):
 class AuthorizedView(LoginRequiredMixin, TemplateView):
     template_name = 'pokedex/authorized.html'
     login_url = '/admin'
+
+class LoginInterfaceView(LoginView):
+    template_name = 'pokedex/login.html'
+
+class LogoutInterfaceView(LogoutView):
+    template_name = 'pokedex/logout.html'
+
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    template_name = 'pokedex/register.html'
+    success_url = '/'
+
+    def get(self, request, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('pokedex_home')
+        return super().get(request,*args,**kwargs)
--- pokemon/models.py
+++ pokemon/models.py
@@ -1,9 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
 
 class Pokemon(models.Model):
-    name = models.CharField(max_length=255)
-    types = models.CharField(max_length=255)
-    text = models.TextField(default='')
+    name = models.CharField(max_length=255, null=True)
+    types = models.CharField(max_length=255, null=True)
+    text = models.TextField(default='',null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="pokemon", null=True)
 
     def __str__(self) -> str:
         return self.name
